095e190d7011210b75f1174b3b298c9740d04df0
Alexey Belous <alexey.belous@sigma.software>
2017-11-10 12:52:40 +0200

    Fix docker-compose config.

--- src/AzureService/AzureService.fs
+++ src/AzureService/AzureService.fs
@@ -1,5 +1,13 @@
-open Suave                 // always open suave
-open Suave.Successful      // for OK-result
-open Suave.Web             // for config
+namespace AzureService
 
-startWebServer defaultConfig (OK "Hello World!")
+module Program = 
+
+    open Suave                 // always open suave
+    open Suave.Successful      // for OK-result
+    open Suave.Web             // for config
+
+    [<EntryPoint>]
+    let main _ = 
+        startWebServer { defaultConfig with bindings = [HttpBinding.createSimple HTTP "0.0.0.0" 80]} (OK "Hello World!")
+        0
+
